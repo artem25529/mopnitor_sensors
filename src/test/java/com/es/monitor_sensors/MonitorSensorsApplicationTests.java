40751ecb29e9a804e1f33b61396914781a7f3e12
artsi <artsi@mail.com>
2025-05-11 20:43:46 +0300

    init

---
+++ src/test/java/com/es/monitor_sensors/MonitorSensorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.monitor_sensors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorSensorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
